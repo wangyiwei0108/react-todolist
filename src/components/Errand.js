2c13776e8e095011487b76e65c40fb86e333bfd1
wangyiwei0108 <wangyiwei0108@gmail.com>
2021-06-12 18:36:35 +0800

    update

--- src/components/Errand.js
+++ src/components/Errand.js
@@ -1,9 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import Rating from 'react-rating';
 
-const Errand = ({todo, todos, setTodos, num}) => {
+const Errand = ({todo, todos, setTodos, num, stars, setCompletedNum}) => {
 
   const [editing, setEditing] = useState(false);
   const [newText, setNewText] = useState('');
+
+  useEffect(() => {
+    const completedTodos = todos.filter(item => {
+      return item.completed === true;
+    })
+    setCompletedNum(completedTodos.length);
+  }, [todos, setCompletedNum])
 
   const doneHandler = () => {
     setTodos(todos.map(item => {
@@ -46,6 +54,19 @@ const Errand = ({todo, todos, setTodos, num}) => {
     setEditing(false);
   }
 
+  const starHandler = (value) => {
+    setTodos(
+      todos.map((item) => {
+        if(item.id === todo.id) {
+          return {
+            ...item, stars: value
+          }
+        }
+        return item
+      })
+    )
+  }
+
   return(
     <div className="errand__container">
       {editing 
@@ -55,15 +76,24 @@ const Errand = ({todo, todos, setTodos, num}) => {
           <input onDoubleClick={confirmHandler} type="text" value={newText} placeholder={todo.text} onChange={newInputHandler}></input>
           <div className="errand__editing-btns">
             <div onClick={confirmHandler} type="submit">
-              <svg><use xlinkHref="images/sprite.svg#icon-circle-with-plus"></use></svg>
+              <svg><use xlinkHref="images/sprite.svg#icon-check"></use></svg>
             </div>
             <div onClick={cancleHandler}>
-              <svg><use xlinkHref="images/sprite.svg#icon-circle-with-cross"></use></svg>
+              <svg><use xlinkHref="images/sprite.svg#icon-cross"></use></svg>
             </div>
           </div>
         </form>
       :
-      <div className="errand__notediting">
+      <div draggable="true" className="errand__notediting">
+        <div className="errand__stars">
+        <Rating
+            emptySymbol={<svg><use xlinkHref="images/sprite.svg#icon-star-outlined"></use></svg>}
+            fullSymbol={<svg><use xlinkHref="images/sprite.svg#icon-star"></use></svg>}
+            quiet={true}
+            stop={3}
+            initialRating={stars}
+            onClick={starHandler}/>
+        </div>
         <div className="errand__notediting-content" onClick={doneHandler}>
           <p className={todo.completed? "errand__completed--num errand__num" : "errand__num"}>{num + 1}</p>
           <p className={todo.completed? "errand__completed--text" : ""}>{todo.text}</p>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
+import Rating from 'react-rating';
 
-const Form = ({setTodos, todos, setStatus}) => {
+const Form = ({setTodos, todos, setStatus, setStars, stars, completedNum}) => {
 
   const [input, setInput] = useState('')
   const [pressedBtnAll, setPressedBtnAll] = useState(true);
@@ -13,7 +14,7 @@ const Form = ({setTodos, todos, setStatus}) => {
 
   const formHandler = (e) => {
     e.preventDefault();
-    setTodos([...todos, {text: input, completed: false, id: Math.random() * 1000}])
+    setTodos([...todos, {text: input, completed: false, id: Math.random() * 1000, stars: stars}])
     setInput('');
   }
 
@@ -38,18 +39,36 @@ const Form = ({setTodos, todos, setStatus}) => {
     setPressedBtnAll(false);
   }
 
+  const starHandler = (value) => {
+    setStars(value);
+  }
+
+  console.log(todos);
+
   return(
     <div className="form__container">
       <form className="form__addbar" onSubmit={formHandler}>
+        <h3>我要完成</h3>
         <input value={input} onChange={inputHandler} type="text"></input>
-        <div onClick={formHandler} type="submit">
-          <svg><use xlinkHref="images/sprite.svg#icon-circle-with-plus"></use></svg>
+        <h3>，重要程度是</h3>
+        <div className="form__rating">
+          <Rating
+            emptySymbol={<svg><use xlinkHref="images/sprite.svg#icon-star-outlined"></use></svg>}
+            fullSymbol={<svg><use xlinkHref="images/sprite.svg#icon-star"></use></svg>}
+            quiet={true}
+            stop={3}
+            onClick={starHandler} />
         </div>
+        <h3>，確定</h3>
+        <div className="form__submit-btn" onClick={formHandler} type="submit">
+          <h3>送出</h3>
+        </div>
+        <h3>。</h3>
       </form>
       <div className="form__filter">
-        <div className={pressedBtnAll?"form__filter-all--pressed": "form__filter-all"} onClick={allHandler}>全部</div>
-        <div className={pressedBtnCom?"form__filter-com--pressed": "form__filter-com"} onClick={completedHandler}>已完成</div>
-        <div className={pressedBtnUncom?"form__filter-uncom--pressed": "form__filter-uncom"} onClick={uncompletedHandler}>未完成</div>
+        <div className={pressedBtnAll?"form__filter-all--pressed": "form__filter-all"} onClick={allHandler}>總共 {todos.length} 項</div>
+        <div className={pressedBtnCom?"form__filter-com--pressed": "form__filter-com"} onClick={completedHandler}>已完成 {completedNum} 項</div>
+        <div className={pressedBtnUncom?"form__filter-uncom--pressed": "form__filter-uncom"} onClick={uncompletedHandler}>未完成 {todos.length - completedNum} 項</div>
       </div>
     </div>
   )
